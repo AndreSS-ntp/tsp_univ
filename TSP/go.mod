83fb260ba61e0c3e761b4976db75b5b49153704a
Bykovskiy Andrey Andreevich <andreybykovs@yandex.ru>
2025-04-28 13:23:03 +0300

    model

---
+++ TSP/go.mod
@@ -0,0 +1,3 @@
+module github.com/AndreSS-ntp/tsp_univ/tree/main/TSP
+
+go 1.23.7
--- TSP/internal/model/city.go
+++ TSP/internal/model/city.go
@@ -1,1 +1,24 @@
 package model
+
+import (
+	"math"
+	"math/rand"
+)
+
+type City struct {
+	X float64
+	Y float64
+}
+
+func NewCity() City {
+	return City{
+		X: rand.Float64() * 100,
+		Y: rand.Float64() * 100,
+	}
+}
+
+func (c City) DistanceTo(other City) float64 {
+	dx := c.X - other.X
+	dy := c.Y - other.Y
+	return math.Sqrt(dx*dx + dy*dy)
+}
