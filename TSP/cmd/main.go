7de4eb71873ceb4a8c9b607edb05c12e38241aaa
Bykovskiy Andrey Andreevich <andreybykovs@yandex.ru>
2025-04-28 13:31:07 +0300

    create main menu

--- TSP/cmd/main.go
+++ TSP/cmd/main.go
@@ -1,1 +1,56 @@
-package cmd
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/AndreSS-ntp/tsp_univ/tree/main/TSP/internal/controller"
+	"github.com/AndreSS-ntp/tsp_univ/tree/main/TSP/internal/view"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	view := view.ConsoleView{}
+	tsp := controller.TSPController{View: view}
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		view.DisplayMenu()
+		input, _ := reader.ReadString('\n')
+		choice, err := strconv.Atoi(strings.TrimSpace(input))
+		if err != nil {
+			fmt.Println("Ошибка: введите число")
+			continue
+		}
+
+		switch choice {
+		case 1:
+			fmt.Print("Введите количество городов: ")
+			countInput, _ := reader.ReadString('\n')
+			count, err := strconv.Atoi(strings.TrimSpace(countInput))
+			if err != nil || count <= 0 {
+				fmt.Println("Ошибка: введите положительное число")
+				continue
+			}
+			tsp.GenerateRandomCities(count)
+			route, dist := tsp.SolveTSP()
+			view.DisplayRoute(route, dist)
+
+		case 2:
+			tsp.InputCities()
+			if len(tsp.Cities) > 0 {
+				route, dist := tsp.SolveTSP()
+				view.DisplayRoute(route, dist)
+			}
+
+		case 3:
+			fmt.Println("Выход...")
+			os.Exit(0)
+
+		default:
+			fmt.Println("Неверный выбор. Попробуйте снова.")
+		}
+	}
+}
--- TSP/internal/controller/tsp.go
+++ TSP/internal/controller/tsp.go
@@ -1,1 +1,100 @@
 package controller
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/AndreSS-ntp/tsp_univ/tree/main/TSP/internal/model"
+	"github.com/AndreSS-ntp/tsp_univ/tree/main/TSP/internal/view"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type TSPController struct {
+	View   view.ConsoleView
+	Cities []model.City
+}
+
+func (tc *TSPController) GenerateRandomCities(count int) {
+	tc.Cities = make([]model.City, count)
+	for i := 0; i < count; i++ {
+		tc.Cities[i] = model.NewCity()
+	}
+	tc.View.DisplayCities(tc.Cities)
+}
+
+func (tc *TSPController) InputCities() {
+	reader := bufio.NewReader(os.Stdin)
+	tc.Cities = []model.City{}
+
+	fmt.Println("Введите координаты городов (x y). Пустая строка - завершение ввода.")
+	for {
+		fmt.Print("Город ", len(tc.Cities)+1, ": ")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSpace(input)
+		if input == "" {
+			break
+		}
+
+		coords := strings.Fields(input)
+		if len(coords) != 2 {
+			fmt.Println("Ошибка: нужно ввести две координаты через пробел")
+			continue
+		}
+
+		x, err1 := strconv.ParseFloat(coords[0], 64)
+		y, err2 := strconv.ParseFloat(coords[1], 64)
+		if err1 != nil || err2 != nil {
+			fmt.Println("Ошибка: координаты должны быть числами")
+			continue
+		}
+
+		tc.Cities = append(tc.Cities, model.City{X: x, Y: y})
+	}
+
+	tc.View.DisplayCities(tc.Cities)
+}
+
+// Алгоритм ближайшего соседа
+func (tc *TSPController) SolveTSP() ([]model.City, float64) {
+	if len(tc.Cities) == 0 {
+		return nil, 0
+	}
+
+	unvisited := make(map[int]bool)
+	for i := range tc.Cities {
+		unvisited[i] = true
+	}
+
+	route := make([]model.City, 0, len(tc.Cities))
+	current := 0
+	delete(unvisited, current)
+	route = append(route, tc.Cities[current])
+
+	totalDistance := 0.0
+
+	for len(unvisited) > 0 {
+		nearest := -1
+		minDist := math.MaxFloat64
+
+		for cityIdx := range unvisited {
+			dist := tc.Cities[current].DistanceTo(tc.Cities[cityIdx])
+			if dist < minDist {
+				minDist = dist
+				nearest = cityIdx
+			}
+		}
+
+		totalDistance += minDist
+		current = nearest
+		delete(unvisited, current)
+		route = append(route, tc.Cities[current])
+	}
+
+	// возврат в начальный город
+	totalDistance += tc.Cities[current].DistanceTo(tc.Cities[0])
+	route = append(route, tc.Cities[0])
+
+	return route, totalDistance
+}
